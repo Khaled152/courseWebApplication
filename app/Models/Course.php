43586f9b5645c8355007c390d5393039ead38975
Khaled152 <khaled.ahmed1512199@gmail.com>
2023-03-30 17:17:22 +0200

    admin view

---
+++ app/Models/Course.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'cover',
+    ];
+    public function users(){
+        $this->belongsToMany(User::class);
+    }
+    public function tags(){
+        $this->belongsToMany(Tag::class);
+    }
+}
